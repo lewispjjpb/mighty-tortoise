2093133aba3d1083ec48a96dc107a6d005ca19e5
Patrick Lewis <lewispjjpb@gmail.com>
2026-05-10 16:02:09 -0400

    Deploy website - based on 5390ad7278abc94bdef45fbf6ee16b177e5b72fc

---
+++ assets/js/19d76f5b.59e7f122.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmighty_toroise=self.webpackChunkmighty_toroise||[]).push([["600"],{4966(t){t.exports=JSON.parse('{"metadata":{"permalink":"/mighty-tortoise/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
